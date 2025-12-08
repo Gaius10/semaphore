660e05cd251d6c1f6e7642c7ff3064475d006248
Ghnafilho <ghna.filho@gmail.com>
2025-12-07 21:12:09 -0300

    Update evolutivos.h

--- lib/evolutivos.h
+++ lib/evolutivos.h
@@ -1,16 +1,29 @@
-#ifndef EVOLUTIVOS_H
-#define EVOLUTIVOS_H
+#ifndef EVOLUCAO_H
+#define EVOLUCAO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
 #define populacao 10
-#define tam_genetico 6
+#define tam_genetico 8
 #define geracoes 45
-char vert_liga(int avaliado[], int carros[]);
-char hori_liga(int avaliado[], int carros[]);
+
+
+
 void genocidio(int pais[][tam_genetico]);
-void predacao_por_sintese(int pai[][tam_genetico],int indice);
+
+void predacao_por_sintese(int pais[][tam_genetico], int indice_pior);
 void mutacao(int filho[]);
 void evolucao(int pai[][tam_genetico], int melhor[]);
+
+
 int avaliacao(int avaliado[]);
 void pior_de_todos(int pais[][tam_genetico]);
+
+
 void darwin(int pais[][tam_genetico], int melhor[]);
+
 void geral();
 #endif
