865e988cb18510a9269577ae5b4c368a68803a09
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-12-09 06:56:38 -0300

    new champions

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(seq 1 30); do
+for i in $(seq 1 100); do
     ./main --mode training --commander specimen_model_01
     clear
     printf "Sleeping 10 seconds to avoid overheat..."
